b22a80956b7d0df496f440361319056b2da440ff
raezon <amardjebabla10@gmail.com>
2023-07-29 19:31:19 +0100

    Refactoring My modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CalculatorModule } from './calculator/calculator.module';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { CalculatorModule } from './modules/calculator/calculator.module';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [CalculatorModule, UserModule, AuthModule],
---
+++ src/database/user.md
@@ -0,0 +1,10 @@
+```{
+  "users": [
+    { "id": 1, "name": "John",
+       // Hashed password for "password123"
+      "password": "$2b$10$YDzmUSq0zlcV6G5zt/86ku/Q5CNmU5xAW2AtAhSZtdl4VhjWFrN3a",
+ },
+    { "id": 2, "name": "Jane", "password": "$2b$10$YDzmUSq0zlcV6G5zt/86ku/Q5CNmU5xAW2AtAhSZtdl4VhjWFrN3a", }
+  ]
+}
+```
